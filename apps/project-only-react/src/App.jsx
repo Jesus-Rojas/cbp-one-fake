b2d9c22f0da36fb63e9b86a883d8834faa3c24d8
Jesus Rojas <jesus@invorious.com>
2024-02-12 18:00:47 -0500

    fix: remove files unnecessary :bug:

--- apps/project-only-react/src/App.jsx
+++ apps/project-only-react/src/App.jsx
@@ -1,30 +1,6 @@
-import { Header } from './components/header/header'
-import { CitaInformation } from './components/cita-information/cita-information'
-import { Viajeros } from './components/viajeros/viajeros'
-import { DetailCita } from './components/detail-cita/detail-cita'
-import { Form } from './components/form/form'
-import { useToggle } from './hooks/useToggle'
-import { TextInformation } from './components/text-information/text-information'
-import { Login } from './components/login/login'
-import { Unauthorized } from './components/unauthorized/unauthorized'
-
 function App() {
-  const { showApp, showForm, showTextInformation } = useToggle();
   return (
-    <>
-      <Login />
-      {/* <Unauthorized /> */}
-      {/* {showForm && <Form />}
-      {showApp && (
-        <>
-          <Header />
-          <CitaInformation />
-          <Viajeros />
-          <DetailCita />
-        </>
-      )}
-      {showTextInformation && <TextInformation />} */}
-    </>
+    <div />
   )
 }
 
