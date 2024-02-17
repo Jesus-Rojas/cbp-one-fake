b8b54950d62d7afea964ba2af7bbc2e5d0be9363
Jesus Rojas <jesus@invorious.com>
2024-02-17 12:58:37 -0500

    fix: update .env.example :sparkles:

--- apps/admin-panel/src/app/modules/rewards/types/limited-reward-config.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface LimitedRewardConfig {
-  min: number;
-  max: number;
-  limit: number;
-  probability: number;
-}
--- apps/admin-panel/src/app/modules/rewards/types/type-reward-config.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum TypeRewardConfig {
-  Coins = 'Coins',
-  Default = 'Default',
-  General = 'General',
-  CustomPrizes = 'CustomPrizes',
-  TokenAttributes = 'TokenAttributes',
-}
